84e18cb16e9e5b8621f3fcea8ca9e79aae93ff78
Kayke Teixeira <kayke.teixeira@gmail.com>
2022-02-02 21:12:43 -0300

    ADDING_CIDR_PUBLIC_ACCESS_IN_THE_CLUSTER

--- modules/master/master.tf
+++ modules/master/master.tf
@@ -4,6 +4,9 @@ resource "aws_eks_cluster" "eks_cluster" {
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
+    public_access_cidrs = [
+      "0.0.0.0/0"
+    ]
     security_group_ids = [
       aws_security_group.cluster-master-sg.id
     ]
