6e61e96db91b4a1cad3f9d718e8b91804d14ff6b
Kayke Teixeira <kayke.teixeira@gmail.com>
2022-03-20 10:58:17 -0300

    ADDING_HELM_PROVIDER

--- provider.tf
+++ provider.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "3.73.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.4.1"
+    }
   }
   required_version = "1.0.8"
 }
@@ -12,3 +17,14 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "helm" {
+  kubernetes {
+    host = module.master.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(module.master.eks_cluster.certificate_authority)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", module.master.eks_cluster]
+      command     = "aws"
+    }
+  }
+}
